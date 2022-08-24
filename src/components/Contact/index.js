35da2ce2fd5d52e1f70db9f352a67c8ff710f41e
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-08-24 13:25:58 -0500

    added state handling to Contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+    
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
 
-// JSX
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
 
 return (
     <section>
         <h1>Contact Me</h1>
-        <form id="contact-form">
+        <form id="contact-form" onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">Name:</label>
-                <input type="text" name="name" />
+                <input type="text" defaultvalue={name} onChange={handleChange} name="name" />
             </div>
            <div>
             <label htmlFor="email">Email Address:</label>
-            <input type="email" name="email"></input>
+            <input type="email" defaultValue={email} onChange={handleChange} name="email"  ></input>
            </div>
            <div>
             <label htmlFor="message">Message:</label>
-            <textarea name="message" rows="5" />
+            <textarea name="message" defaultValue={message} onChange={handleChange} rows="5"  />
            </div>
            <button type="submit">Submit</button>
         </form>
