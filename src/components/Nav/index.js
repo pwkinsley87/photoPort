fd5538212d02ab2592b7685bf3136a59b3e9fcf6
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-08-24 11:03:14 -0500

    wrote tests for Nav section; 1 failing

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,37 +1,44 @@
 import React from 'react';
 
-const categories = [
-    {
-        name: "Commercial",
-        description: 
-            "Photos of grocery stores, food trucks, and other commercial projects.",
-    },
-    { name: "Portraits", description: "Portraits of the people in my life." },
-    { name: "Food", description: "Delicious delicacies." },
-    { name: "Landscapes", description: "The beauty of nature." },
-];
-
-function categorySelected(name) {
-    console.log(`${name} clicked`)
-};
-
 function Nav() {
+
+
+    const categories = [
+        {
+            name: "Commercial",
+            description: 
+                "Photos of grocery stores, food trucks, and other commercial projects.",
+        },
+        { name: "Portraits", description: "Portraits of the people in my life." },
+        { name: "Food", description: "Delicious delicacies." },
+        { name: "Landscapes", description: "The beauty of nature." },
+    ];
+
+    function categorySelected(name) {
+        console.log(`${name} clicked`)
+        return ClipboardItem;
+    };
+
     return (
-        <header>
+        <header className="flex-row px-1">
             <h2>
-                <a href="/">
-                    <span role="img" aria-label="camera"></span>Oh Snap!
+                <a data-testid="link" href="/"> 
+                    <span role="img" aria-label="camera"> 📷</span>
+                    Oh Snap!
                 </a>
             </h2>
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a href="#about">
+                        <a data-testid="about" href="#about" onClick=
+                         {() => handleClick("About")} >
                             About Me
                         </a>
                     </li>
-                    <li>
-                        <span>Contact</span>
+                    <li className={"mx-2"}>
+                        <span onClick={() => handleClick   ('Contact')}>
+                          Contact
+                        </span>
                     </li>
                     {categories.map((category) => (
                         <li classname="mx-1" key={category.name} >
